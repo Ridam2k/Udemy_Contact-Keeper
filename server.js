316e14eafa53f3413f8f6e69f200daa9031e03fa
Ridam <ridam2k@gmail.com>
2021-04-17 20:51:52 +0530

    heroku installation commands

--- server.js
+++ server.js
@@ -1,23 +1,32 @@
-const express = require('express'); //Importing express
+const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 
-//Connect Databse
+//Remove initial route
+
+// Connect Database
 connectDB();
 
-//Init middleware
+// Init Middleware
 app.use(express.json({ extended: false }));
 
-const PORT = process.env.port || 5000;
-
-app.get('/', (req, res) =>
-	res.json({ msg: 'Welcome to the ContactKeeper API...' })
-);
-
-//Define Routes
+// Define Routes
 app.use('/api/users', require('./routes/users'));
-app.use('/api/contacts', require('./routes/contacts'));
 app.use('/api/auth', require('./routes/auth'));
+app.use('/api/contacts', require('./routes/contacts'));
 
-app.listen(PORT, () => console.log(`Server started on ${PORT}`));
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+	// Set static folder
+	app.use(express.static('client/build'));
+
+	app.get('*', (req, res) =>
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+	);
+}
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
